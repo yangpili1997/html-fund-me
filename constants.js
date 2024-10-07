abf232b13d3e80e4ae0b1cab822b7aff20256eab
yangli <yangpili1997@gmail.com>
2024-10-07 18:33:42 +0800

    前端

--- constants.js
+++ constants.js
@@ -1,6 +1,6 @@
 // export const zkSyncContractAddress = "0x4B5DF730c2e6b28E17013A1485E5d9BC41Efe021"
 // export const contractAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
-export const contractAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
+export const contractAddress = "0xa1832fA94a81acc653A7b8145784D50A3A1f0e17"
 export const abi = [
   {
     inputs: [
